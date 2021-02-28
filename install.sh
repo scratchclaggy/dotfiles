e49dba7f2a4b97337467ad608fc0f63cbaa69eaa
James <james@jamesvandeven.com>
2021-02-28 17:58:52 +0900

    Made install.sh executable

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-#!/bin/bash/env bash
+#!/bin/bash
 
 # Set-up symlinks
 sudo ln -s ~/.dotfiles/bashrc ~/.bashrc
 sudo ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
 sudo ln -s ~/.dotfiles/gitconfig ~/.gitconfig
-sudo ln -s ~/.dotfiles/nvim ~/.config/nvim
+sudo ln -s ~/.dotfiles/init.vim ~/.config/nvim/init.vim
+sudo ln -s ~/.dotfiles/settings ~/.config/nvim/settings
 
