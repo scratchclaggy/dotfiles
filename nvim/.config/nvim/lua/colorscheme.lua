fe210d7dd2ce186837dee9b9698b1484beb84bda
James van de Ven <hello@jamesvandeven.com>
2022-01-10 21:30:51 +0800

    Moved to submodule repo for nvim settings

--- nvim/.config/nvim/lua/colorscheme.lua
+++
@@ -1,2 +0,0 @@
-vim.api.nvim_set_option("termguicolors", true)
-vim.api.nvim_command("colorscheme dracula")
--- nvim/.config/nvim/lua/snippet.lua
+++
@@ -1,1 +0,0 @@
-vim.g.vsnip_snippet_dir = "~/.local/share/nvim/site/pack/packer/start/friendly-snippets/snippets/"
