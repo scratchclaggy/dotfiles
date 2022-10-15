13415b36973041c6d63813c0650af6e3752f2123
James van de Ven <hello@jamesvandeven.com>
2022-10-15 13:40:50 +0800

    Lualine winbar + spaces for stylua

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,39 +1,44 @@
 local lspconfig = require("lspconfig")
 local servers = {
-	{ language = "bashls" },
-	{ language = "clangd" },
-	{ language = "gopls" },
-	{ language = "prismals" },
-	{ language = "pyright" },
-	{ language = "rust_analyzer" },
-	{ language = "tailwindcss" },
-	{ language = "texlab" },
-	{ language = "tsserver" },
+    { language = "bashls" },
+    { language = "clangd" },
+    { language = "gopls" },
+    { language = "prismals" },
+    { language = "pyright" },
+    { language = "rust_analyzer" },
+    { language = "tailwindcss" },
+    { language = "texlab" },
+    { language = "tsserver" },
 }
 
 local signs = { Error = "!", Warn = "!", Hint = "?", Info = "?" }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 for _, lsp in ipairs(servers) do
-	lspconfig[lsp.language].setup({
-		capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-	})
+    lspconfig[lsp.language].setup({
+        capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+    })
 end
 
 local null_ls = require("null-ls")
 null_ls.setup({
-	sources = {
-		null_ls.builtins.formatting.black,
-		null_ls.builtins.formatting.clang_format.with({
-			extra_args = { "--style", "{BasedOnStyle: Google, ColumnLimit: 0, IndentWidth: 4}" },
-		}),
-		null_ls.builtins.formatting.prettier,
-		null_ls.builtins.formatting.rustfmt,
-		null_ls.builtins.formatting.stylua,
-	},
+    sources = {
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.clang_format.with({
+            extra_args = { "--style", "{BasedOnStyle: Google, ColumnLimit: 0, IndentWidth: 4}" },
+        }),
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.rustfmt,
+        null_ls.builtins.formatting.stylua.with({
+            extra_args = {
+                "--indent-type",
+                "Spaces",
+            },
+        }),
+    },
 })
 
 require("trouble").setup()
--- nvim/.config/nvim/lua/plugin_manager.lua
+++ nvim/.config/nvim/lua/plugin_manager.lua
@@ -2,94 +2,94 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-	vim.cmd("packadd packer.nvim")
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd("packadd packer.nvim")
 end
 
 return require("packer").startup(function()
-	-- Plugin Manager
-	use("wbthomason/packer.nvim")
+    -- Plugin Manager
+    use("wbthomason/packer.nvim")
 
-	-- LSP
-	use("neovim/nvim-lspconfig")
-	use({ "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" })
-	use({ "yioneko/nvim-yati", requires = "nvim-treesitter/nvim-treesitter" })
-	use({
-		"folke/trouble.nvim",
-		requires = "kyazdani42/nvim-web-devicons",
-	})
-	use { 'glepnir/lspsaga.nvim' }
+    -- LSP
+    use("neovim/nvim-lspconfig")
+    use({ "jose-elias-alvarez/null-ls.nvim", requires = "nvim-lua/plenary.nvim" })
+    use({ "yioneko/nvim-yati", requires = "nvim-treesitter/nvim-treesitter" })
+    use({
+        "folke/trouble.nvim",
+        requires = "kyazdani42/nvim-web-devicons",
+    })
+    use({ "glepnir/lspsaga.nvim" })
 
-	-- Completion
-	use("hrsh7th/nvim-cmp")
-	use("hrsh7th/cmp-buffer")
-	use("hrsh7th/cmp-nvim-lsp")
-	use("hrsh7th/cmp-nvim-lua")
-	use("hrsh7th/cmp-path")
-	use("hrsh7th/cmp-vsnip")
+    -- Completion
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-nvim-lsp")
+    use("hrsh7th/cmp-nvim-lua")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-vsnip")
 
-	-- Syntax
-	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
-	use({ "nvim-treesitter/nvim-treesitter-refactor", requires = "nvim-treesitter/nvim-treesitter" })
-	use("JoosepAlviste/nvim-ts-context-commentstring")
-	use("kylechui/nvim-surround")
-	use("tpope/vim-sleuth")
+    -- Syntax
+    use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+    use({ "nvim-treesitter/nvim-treesitter-refactor", requires = "nvim-treesitter/nvim-treesitter" })
+    use("JoosepAlviste/nvim-ts-context-commentstring")
+    use("kylechui/nvim-surround")
+    use("tpope/vim-sleuth")
 
-	-- Snippet
-	use("hrsh7th/vim-vsnip")
-	use("hrsh7th/vim-vsnip-integ")
-	use("rafamadriz/friendly-snippets")
+    -- Snippet
+    use("hrsh7th/vim-vsnip")
+    use("hrsh7th/vim-vsnip-integ")
+    use("rafamadriz/friendly-snippets")
 
-	-- Fuzzy Finder
+    -- Fuzzy Finder
 
-	use({ "nvim-telescope/telescope.nvim", requires = "nvim-lua/plenary.nvim" })
+    use({ "nvim-telescope/telescope.nvim", requires = "nvim-lua/plenary.nvim" })
 
-	-- Color
-	use("norcalli/nvim-colorizer.lua")
+    -- Color
+    use("norcalli/nvim-colorizer.lua")
 
-	-- Colorscheme
-	use({ "dracula/vim", as = "dracula" })
-	use({ "catppuccin/nvim", as = "catppuccin" })
+    -- Colorscheme
+    use({ "dracula/vim", as = "dracula" })
+    use({ "catppuccin/nvim", as = "catppuccin" })
 
-	-- Icon
-	use("kyazdani42/nvim-web-devicons")
+    -- Icon
+    use("kyazdani42/nvim-web-devicons")
 
-	-- Utility
-	use("jghauser/mkdir.nvim")
-	use("tpope/vim-eunuch")
-	use("tpope/vim-unimpaired")
+    -- Utility
+    use("jghauser/mkdir.nvim")
+    use("tpope/vim-eunuch")
+    use("tpope/vim-unimpaired")
 
-	-- Neovim Lua Development
-	use("nvim-lua/plenary.nvim")
+    -- Neovim Lua Development
+    use("nvim-lua/plenary.nvim")
 
-	-- Statusline
-	use({ "nvim-lualine/lualine.nvim", requires = "kyazdani42/nvim-web-devicons" })
+    -- Statusline
+    use({ "nvim-lualine/lualine.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
-	-- Indent
-	use("lukas-reineke/indent-blankline.nvim")
+    -- Indent
+    use("lukas-reineke/indent-blankline.nvim")
 
-	-- Git
-	use({ "tpope/vim-fugitive" })
-	use({ "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" })
-	use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
+    -- Git
+    use({ "tpope/vim-fugitive" })
+    use({ "lewis6991/gitsigns.nvim", requires = "nvim-lua/plenary.nvim" })
+    use({ "TimUntersberger/neogit", requires = "nvim-lua/plenary.nvim" })
 
-	-- Comment
-	use("numToStr/Comment.nvim")
-	use({ "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" })
+    -- Comment
+    use("numToStr/Comment.nvim")
+    use({ "folke/todo-comments.nvim", requires = "nvim-lua/plenary.nvim" })
 
-	-- Session
-	use("windwp/nvim-autopairs")
-	use("windwp/nvim-ts-autotag")
+    -- Session
+    use("windwp/nvim-autopairs")
+    use("windwp/nvim-ts-autotag")
 
-	-- Session
-	use("rmagatti/auto-session")
+    -- Session
+    use("rmagatti/auto-session")
 
-	-- Keybinding
-	use("LionC/nest.nvim")
-	use("svermeulen/vim-cutlass")
-	use("svermeulen/vim-yoink")
-	use("svermeulen/vim-subversive")
+    -- Keybinding
+    use("LionC/nest.nvim")
+    use("svermeulen/vim-cutlass")
+    use("svermeulen/vim-yoink")
+    use("svermeulen/vim-subversive")
 
-	-- Split and Window
-	use("beauwilliams/focus.nvim")
+    -- Split and Window
+    use("beauwilliams/focus.nvim")
 end)
