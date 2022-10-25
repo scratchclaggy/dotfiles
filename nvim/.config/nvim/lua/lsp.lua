fcf4e695c318cae8d99e80db6fde488015ab2825
James van de Ven <hello@jamesvandeven.com>
2022-10-25 08:33:59 +0800

    Fix nvim cmp deprecated setup

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -16,10 +16,10 @@ for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
-
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 for _, lsp in ipairs(servers) do
     lspconfig[lsp.language].setup({
-        capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        capabilities = capabilities,
     })
 end
 
