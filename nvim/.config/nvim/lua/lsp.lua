c2a52d1c690759a51612d035370a02643b4cead0
James van de Ven <hello@jamesvandeven.com>
2022-12-09 13:00:54 +0800

    Enabled sumneko

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -11,14 +11,39 @@ local servers = {
     { language = "tsserver" },
 }
 
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+capabilities.offsetEncoding = "utf-8"
+
+require("lspconfig").sumneko_lua.setup({
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim" },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+
 local signs = { Error = "!", Warn = "!", Hint = "?", Info = "?" }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
-capabilities.offsetEncoding = 'utf-8'
 for _, lsp in ipairs(servers) do
     lspconfig[lsp.language].setup({
         capabilities = capabilities,
