592c79aabfed07cf01fd51d52e03ad16f1058966
James van de Ven <hello@jamesvandeven.com>
2023-02-15 18:30:23 +0800

    Fix cutlass + move to lazygit

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,21 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  opts = opts or {}
+  opts.silent = opts.silent ~= false
+  vim.keymap.set(mode, lhs, rhs, opts)
+end
+
+local plug = require("utilities").plug
+
+map({ "n", "x" }, "m", "d")
+map("n", "mm", "dd")
+map("n", "M", "D")
+map("n", "Y", "y$")
+map({ "n", "x" }, "s", plug("SubversiveSubstitute"))
+map("n", "ss", plug("SubversiveSubstituteLine"))
+map("n", "S", plug("SubversiveSubstituteToEndOfLine"))
+map("n", "p", plug("YoinkPaste_p"))
+map("x", "p", plug("SubversiveSubstitute"))
+map("n", "P", plug("YoinkPaste_P"))
+map("x", "P", plug("SubversiveSubstitute"))
+map("n", "<c-n>", plug("YoinkPostPasteSwapBack"))
+map("n", "<c-p>", plug("YoinkPostPasteSwapForward"))
--- nvim/.config/nvim/lua/plugins/cutlass.lua
+++ nvim/.config/nvim/lua/plugins/cutlass.lua
@@ -1,34 +1,17 @@
-local plug = require("utilities").plug
-
-vim.g.yoinkIncludeDeleteOperations = 1
-
 return {
   {
     "svermeulen/vim-cutlass",
-    keys = {
-      { "m", "d", { mode = "nx" } },
-      { "mm", "dd" },
-      { "M", "D" },
-    },
+    lazy = false,
   },
   {
     "svermeulen/vim-subversive",
-    keys = {
-      { "s", plug("SubversiveSubstitute"), { mode = "ns" } },
-      { "ss", plug("SubversiveSubstituteLine") },
-      { "S", plug("SubversiveSubstituteToEndOfLine") },
-      { "p", plug("SubversiveSubstitute"), { mode = "s" } },
-      { "P", plug("SubversiveSubstitute"), { mode = "s" } },
-    },
+    lazy = false,
   },
   {
     "svermeulen/vim-yoink",
-    keys = {
-      { "p", plug("YoinkPaste_p") },
-      { "P", plug("YoinkPaste_P") },
-      { "Y", "y$" },
-      { "<c-n>", plug("YoinkPostPasteSwapBack") },
-      { "<c-p>", plug("YoinkPostPasteSwapForward") },
-    },
+    config = function()
+      vim.g.yoinkIncludeDeleteOperations = 1
+    end,
+    lazy = false,
   },
 }
