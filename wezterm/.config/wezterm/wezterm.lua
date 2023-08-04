9d07f98484be6d08e339b870772c790b7bbe389c
James van de Ven <james.vandeven@woodside.com.au>
2023-08-04 12:50:40 +0800

    wezterm keybinds

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,4 +4,50 @@ return {
 	color_scheme = "Catppuccin Mocha",
 	font_size = 16,
 	font = wezterm.font("JetbrainsMono Nerd Font"),
+
+	-- timeout_milliseconds defaults to 1000 and can be omitted
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 },
+	keys = {
+		{
+			-- Split Vertical
+			key = "-",
+			mods = "LEADER",
+			action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+		},
+		{
+			-- Split Horizontal
+			key = "|",
+			mods = "LEADER|SHIFT",
+			action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+		},
+
+		-- Activate Pane HJKL
+		{
+			key = "h",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Left"),
+		},
+		{
+			key = "j",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Down"),
+		},
+		{
+			key = "k",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Up"),
+		},
+		{
+			key = "l",
+			mods = "LEADER",
+			action = wezterm.action.ActivatePaneDirection("Right"),
+		},
+
+		{
+			-- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+			key = "a",
+			mods = "LEADER|CTRL",
+			action = wezterm.action.SendKey({ key = "a", mods = "CTRL" }),
+		},
+	},
 }
