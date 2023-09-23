c47d19384ab3a6bab5b6da0982b205b4c84b151f
James van de Ven <hello@jamesvandeven.com>
2023-09-23 13:29:21 +0800

    tidying

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,20 +1,9 @@
-local Util = require("lazyvim.util")
-local function map(mode, lhs, rhs, opts)
-  opts = opts or {}
-  opts.silent = opts.silent ~= false
-  vim.keymap.set(mode, lhs, rhs, opts)
-end
-
-local plug = require("utilities").plug
-local cmd = require("utilities").cmd
-
+local set = vim.keymap.set
 local neogit = require("neogit")
-local gs = require("gitsigns")
+local gitsigns = require("gitsigns")
 
-map("n", "<leader>gg", neogit.open, { desc = "Neogit" })
-map("n", "<leader>gl", function()
-  Util.float_term({ "lazygit" }, { cwd = Util.get_root() })
-end, { desc = "Lazygit" })
-map("n", "<leader>gG", "<nop>")
-map("n", "<leader>ug", gs.toggle_deleted, { desc = "Toggle Git Deleted" })
-map("n", "<leader>ub", gs.toggle_current_line_blame, { desc = "Toggle Git Deleted" })
+set("n", "<leader>gg", neogit.open, { desc = "Neogit" })
+set("n", "<leader>gG", "<nop>")
+
+set("n", "<leader>ug", gitsigns.toggle_deleted, { desc = "Toggle Git Deleted" })
+set("n", "<leader>ub", gitsigns.toggle_current_line_blame, { desc = "Toggle Git Deleted" })
