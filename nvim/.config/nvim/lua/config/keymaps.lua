f69689f82f9f5e9877d0929201803548491687a2
James van de Ven <hello@jamesvandeven.com>
2023-10-16 19:39:17 +0800

    telescope keymaps for trouble.nvim

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -7,3 +7,16 @@ set("n", "<leader>gG", "<nop>")
 
 set("n", "<leader>ug", gitsigns.toggle_deleted, { desc = "Toggle Git Deleted" })
 set("n", "<leader>ub", gitsigns.toggle_current_line_blame, { desc = "Toggle Git Deleted" })
+
+local trouble = require("trouble.providers.telescope")
+
+local telescope = require("telescope")
+
+telescope.setup({
+  defaults = {
+    mappings = {
+      i = { ["<c-t>"] = trouble.open_with_trouble },
+      n = { ["<c-t>"] = trouble.open_with_trouble },
+    },
+  },
+})
