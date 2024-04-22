7e5e1090593c3dd7e08c97b05c3bf8c5aa526ed2
James van de Ven <hello@jamesvandeven.com>
2024-04-22 21:18:31 +0800

    Fix weird indenting

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -9,20 +9,14 @@ return {
       'javascript',
       'lua',
       'markdown',
+      'query',
       'tsx',
       'typescript',
       'vim',
       'vimdoc',
     },
     auto_install = true,
-    highlight = {
-      enable = true,
-      -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-      --  If you are experiencing weird indenting issues, add the language to
-      --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-      additional_vim_regex_highlighting = { 'ruby' },
-    },
-    indent = { enable = true, disable = { 'ruby' } },
+    highlight = true,
   },
   config = function(_, opts)
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
