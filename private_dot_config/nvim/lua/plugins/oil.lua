c43475a05241c01bf300bc52ad64982dc6cf26d6
James van de Ven <hello@jamesvandeven.com>
2024-04-22 21:08:32 +0800

    Lazy loading

--- private_dot_config/nvim/lua/plugins/oil.lua
+++ private_dot_config/nvim/lua/plugins/oil.lua
@@ -1,7 +1,11 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = {},
+  opts = { default_file_explorer = true, keymaps = {
+    ['<C-h>'] = false,
+    ['<C-l>'] = false,
+  } },
+  lazy = false,
   keys = {
     { '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' } },
   },
