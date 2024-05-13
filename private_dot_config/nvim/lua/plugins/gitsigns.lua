915e679728b44b6160aade2b6bc9512db54a1a0a
James van de Ven <james.vandeven@woodside.com>
2024-05-13 15:10:52 +0800

    Use unimpaired

--- private_dot_config/nvim/lua/plugins/gitsigns.lua
+++ private_dot_config/nvim/lua/plugins/gitsigns.lua
@@ -1,25 +1,54 @@
 return {
   'lewis6991/gitsigns.nvim',
   opts = {
-    signs = {
-      add = { text = '+' },
-      change = { text = '~' },
-      delete = { text = '_' },
-      topdelete = { text = '‾' },
-      changedelete = { text = '~' },
-    },
-    on_attach = function(buffer)
-      local gs = package.loaded.gitsigns
+    on_attach = function(bufnr)
+      local gitsigns = require 'gitsigns'
 
-      local function map(mode, l, r, desc)
-        vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
       end
 
-      map('n', ']h', gs.next_hunk, 'Next Hunk')
-      map('n', '[h', gs.prev_hunk, 'Prev Hunk')
-      map({ 'n', 'v' }, '<leader>ghs', ':Gitsigns stage_hunk<CR>', 'Stage Hunk')
-      map({ 'n', 'v' }, '<leader>ghr', ':Gitsigns reset_hunk<CR>', 'Reset Hunk')
-      map('n', '<leader>ghR', gs.reset_buffer, 'Reset Buffer')
+      -- Navigation
+      map('n', ']h', function()
+        if vim.wo.diff then
+          vim.cmd.normal { ']h', bang = true }
+        else
+          gitsigns.nav_hunk 'next'
+        end
+      end, { desc = 'Jump to next git [h]unk' })
+
+      map('n', '[h', function()
+        if vim.wo.diff then
+          vim.cmd.normal { '[h', bang = true }
+        else
+          gitsigns.nav_hunk 'prev'
+        end
+      end, { desc = 'Jump to previous git [h]unk' })
+
+      -- Actions
+      -- visual mode
+      map('v', '<leader>hs', function()
+        gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = 'stage git hunk' })
+      map('v', '<leader>hr', function()
+        gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+      end, { desc = 'reset git hunk' })
+      -- normal mode
+      map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+      map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+      map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+      map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
+      map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+      map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+      map('n', 'yob', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
+      map('n', '<leader>hb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+      map('n', '<leader>hd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+      map('n', '<leader>hD', function()
+        gitsigns.diffthis '@'
+      end, { desc = 'git [D]iff against last commit' })
+      map('n', 'yod', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
     end,
   },
 }
---
+++ private_dot_config/nvim/lua/plugins/unimpaired.lua
@@ -0,0 +1,12 @@
+return {
+  'tummetott/unimpaired.nvim',
+  event = 'VeryLazy',
+  opts = {
+    enable_background = false,
+    disable_background = false,
+    toggle_background = false,
+    enable_diff = false,
+    disable_diff = false,
+    toggle_diff = false,
+  },
+}
