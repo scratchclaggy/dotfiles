c166e823d4340fd96b4e2aa1b9082ed5ae15cbbe
James van de Ven <james.vandeven@woodside.com>
2024-12-02 16:11:59 +0800

    view hidden in oil

--- private_dot_config/nvim/lua/plugins/oil.lua
+++ private_dot_config/nvim/lua/plugins/oil.lua
@@ -1,10 +1,14 @@
 return {
   'stevearc/oil.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
-  opts = { default_file_explorer = true, keymaps = {
-    ['<C-h>'] = false,
-    ['<C-l>'] = false,
-  } },
+  opts = {
+    default_file_explorer = true,
+    keymaps = {
+      ['<C-h>'] = false,
+      ['<C-l>'] = false,
+    },
+    view_options = { show_hidden = true },
+  },
   lazy = false,
   keys = {
     { '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' } },
