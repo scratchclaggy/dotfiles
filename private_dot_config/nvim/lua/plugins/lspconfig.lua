d604400585a7f87c9ecf94c78b5c6ff5bae79034
James van de Ven <james.vandeven@woodside.com>
2024-12-02 16:12:06 +0800

    lazydev

--- private_dot_config/nvim/lua/plugins/lspconfig.lua
+++ private_dot_config/nvim/lua/plugins/lspconfig.lua
@@ -11,7 +11,7 @@ return {
 
     -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
     -- used for completion, annotations and signatures of Neovim apis
-    { 'folke/neodev.nvim', opts = {} },
+    -- { 'folke/neodev.nvim', opts = {} },
   },
   config = function()
     vim.api.nvim_create_autocmd('LspAttach', {
