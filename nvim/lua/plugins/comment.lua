99ad2b5b244266ee47b85d2f6e4490822a0118e4
James van de Ven <hello@jamesvandeven.com>
2024-12-31 13:47:24 +0800

    react commentstring

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,1 +1,14 @@
-return { 'numToStr/Comment.nvim', opts = {} }
+return {
+  'numToStr/Comment.nvim',
+  opts = function()
+    local ts_comment = require 'ts_context_commentstring.integrations.comment_nvim'
+
+    return {
+      pre_hook = ts_comment.create_pre_hook(),
+    }
+  end,
+  dependencies = { {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = { enable_autocmd = false },
+  } },
+}
