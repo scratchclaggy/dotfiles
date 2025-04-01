284a15c92670557fb334c99a3a61ee3c4de0fbe9
James van de Ven <hello@jamesvandeven.com>
2025-04-01 08:44:25 +0800

    remove mini.surround

--- nvim/lua/plugins/surround.lua
+++ nvim/lua/plugins/surround.lua
@@ -1,1 +1,6 @@
-return { 'echasnovski/mini.surround', version = false, config = true }
+return {
+  'kylechui/nvim-surround',
+  version = '^3.0.0', -- Use for stability; omit to use `main` branch for the latest features
+  event = 'VeryLazy',
+  opts = {},
+}
