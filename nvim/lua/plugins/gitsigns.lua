c452b9006023f1a8063a3b9a8b9c36a3bfbd713a
James van de Ven <hello@jamesvandeven.com>
2025-06-05 14:45:59 +1000

    updated gitsigns keys

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -39,7 +39,6 @@ return {
       map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
       map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
       map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-      map('n', '<leader>hu', gitsigns.undo_stage_hunk, { desc = 'git [u]ndo stage hunk' })
       map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
       map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
       map('n', 'yob', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
@@ -48,7 +47,7 @@ return {
       map('n', '<leader>hD', function()
         gitsigns.diffthis '@'
       end, { desc = 'git [D]iff against last commit' })
-      map('n', 'yod', gitsigns.toggle_deleted, { desc = '[T]oggle git show [D]eleted' })
+      map('n', 'yod', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
     end,
   },
 }
