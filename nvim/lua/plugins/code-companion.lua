60f9d1dea86ad69a5382e098353d2dfadabe76d4
James van de Ven <hello@jamesvandeven.com>
2025-06-05 14:45:45 +1000

    use 4o

--- nvim/lua/plugins/code-companion.lua
+++ nvim/lua/plugins/code-companion.lua
@@ -1,13 +1,25 @@
 return {
   {
     'olimorris/codecompanion.nvim',
-    opts = {},
+    opts = {
+      opts = {
+        strategies = {
+          chat = { adapter = 'githubmodels', model = 'gpt-4o' },
+          inline = { adapter = 'githubmodels', model = 'gpt-4o' },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('codecompanion').setup(opts)
+
+      vim.cmd [[cab cc CodeCompanion]]
+    end,
+
     keys = {
-      { '<C-a>', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' } },
-      { '<LocalLeader>a', '<cmd>CodeCompanionChat Toggle<cr>', mode = { 'n', 'v' } },
+      { '<C-c>', '<cmd>CodeCompanionActions<cr>', mode = { 'n', 'v' } },
+      { '<LocalLeader>c', '<cmd>CodeCompanionChat Toggle<cr>', mode = { 'n', 'v' } },
       { 'ga', '<cmd>CodeCompanionChat Add<cr>', mode = 'v' },
     },
-
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-treesitter/nvim-treesitter',
@@ -17,8 +29,6 @@ return {
     'zbirenbaum/copilot.lua',
     event = 'InsertEnter',
     cmd = 'Copilot',
-    config = function()
-      require('copilot').setup {}
-    end,
+    opts = {},
   },
 }
